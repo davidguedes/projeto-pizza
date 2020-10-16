edfea71d289d043bd786915bfde5b0d868f0be10
David Guedes <d2vid.guedes@gmail.com>
2020-10-15 23:02:17 -0300

    Configurando botão de adicionar ao carrinho

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+let cart = [];
 let ModalQt = 1;
+let modalKey;
+
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
@@ -15,6 +18,7 @@ pizzaJson.map((item, index) => {
     e.preventDefault();
     let key = e.target.closest('.pizza-item').getAttribute('data-key');
     modalQt = 1;
+    modalKey = key;
 
     c('.pizzaBig img').src = pizzaJson[key].img;
     c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -66,4 +70,14 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
     c('.pizzaInfo--size.selected').classList.remove('selected');
     size.classList.add('selected');
   });
+});
+c('.pizzaInfo--addButton').addEventListener('click', () => {
+  let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+  cart.push({
+    id: pizzaJson[modalKey].id,
+    size: size,
+    qt: modalQt
+  });
+
+  closeModal();
 });
